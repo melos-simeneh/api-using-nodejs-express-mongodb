9d38c62f499e88e96132715d557c3cd2a15e2fbc
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-19 12:01:55 -0700

    Delete tour endpoint

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/v1/tours", (req, res) => {
-  db.query("SELECT * FROM tours", (err, results, fields) => {
+  db.query("SELECT * FROM tours", (err, results) => {
     if (err)
       return res
         .status(500)
-        .json({ status: "error", message: "Failed to fetch data" });
+        .json({ status: "error", message: "Failed to fetch data", error: err });
 
     res.json({
       status: "success",
@@ -26,7 +26,7 @@ app.post("/api/v1/tours", (req, res) => {
     if (err)
       return res
         .status(500)
-        .json({ status: "error", message: "Failed to save tour" });
+        .json({ status: "error", message: "Failed to save tour", error: err });
     const id = results.insertId;
     res.json({
       status: "success",
@@ -42,7 +42,13 @@ app.get("/api/v1/tours/:id", (req, res) => {
     if (err)
       return res
         .status(500)
-        .json({ status: "error", message: "Failed to get a tour" });
+        .json({ status: "error", message: "Failed to get a tour", error: err });
+
+    if (result.length === 0)
+      return res.status(400).json({
+        status: "fail",
+        message: `Tour with id ${id} not found`,
+      });
 
     res.json({
       status: "success",
@@ -54,12 +60,19 @@ app.get("/api/v1/tours/:id", (req, res) => {
 app.patch("/api/v1/tours/:id", (req, res) => {
   const id = req.params.id;
   const tour = req.body;
-
-  db.query("UPDATE tours SET ? WHERE ?", [tour, id], (err, results) => {
+  db.query("UPDATE tours SET ? WHERE ?", [tour, { id }], (err, result) => {
     if (err)
-      return res
-        .status(500)
-        .json({ status: "error", message: "Failed to update a tour" });
+      return res.status(500).json({
+        status: "error",
+        message: "Failed to update a tour",
+        error: err,
+      });
+
+    if (result.affectedRows === 0)
+      return res.status(400).json({
+        status: "fail",
+        message: `Tour with id ${id} not found`,
+      });
 
     res.json({
       status: "success",
@@ -68,6 +81,29 @@ app.patch("/api/v1/tours/:id", (req, res) => {
   });
 });
 
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.query("DELETE FROM tours WHERE ?", { id }, (err, result) => {
+    if (err)
+      return res.status(500).json({
+        status: "error",
+        message: "Failed to delete a tour",
+        error: err,
+      });
+
+    if (result.affectedRows === 0)
+      return res.status(400).json({
+        status: "fail",
+        message: `Tour with id ${id} not found`,
+      });
+
+    res.json({
+      status: "success",
+      message: "Tour deleted",
+    });
+  });
+});
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
