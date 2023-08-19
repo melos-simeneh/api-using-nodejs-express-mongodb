32cb7e1163486b31c3aa8fcf6b9bc1b1e43a228a
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-19 11:33:23 -0700

    Create Tour endpoint

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const db = require("./db");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/api/v1/tours", (req, res) => {
   db.query("SELECT * FROM tours", (err, results, fields) => {
     if (err)
@@ -14,6 +16,22 @@ app.get("/api/v1/tours", (req, res) => {
       status: "success",
       results: results.length,
       data: { tours: results },
+    });
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const tour = req.body;
+  db.query("INSERT INTO tours SET ?", tour, (err, results) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ status: "error", message: "Failed to save tour" });
+    const id = results.insertId;
+    res.json({
+      status: "success",
+      message: "Tour created",
+      data: { tour: { id, ...tour } },
     });
   });
 });
